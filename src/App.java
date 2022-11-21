15a97a6166000c5e4a9e584e0a4544b08c6691f4
itsxSAJO <118242127+itsxSAJO@users.noreply.github.com>
2022-11-21 18:56:35 -0500

    Update App.java
    ingreso de nombre, Said Luna

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Anthony...!");
+        System.out.println("Hola mi nombre es Said Luna");
     }
 }

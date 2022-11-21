4f188d2f84f4f9fe633210797521e1b203bb51e5
Tonga <118144420+TonguitaXd@users.noreply.github.com>
2022-11-20 22:17:19 -0500

    Borrar los class del prj

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Tongaaa...!");
+        System.out.println("Hello, Anthony...!");
     }
 }

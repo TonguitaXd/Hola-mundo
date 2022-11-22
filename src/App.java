375bdb1537b18712aa3ccb3066f0584f3e8210a5
AnaelMolina <94010444+AnaelMolina@users.noreply.github.com>
2022-11-21 19:57:11 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Anthony...!");
         System.out.println("Hola mi nombre es Said Luna");
+        System.out.println("Hola mi nombre es Anael");
+        
     }
 }
